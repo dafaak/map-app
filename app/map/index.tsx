5b93e837155f359f1e9c5ca42ede984a6f037666
Israel <israel.pe@outlook.es>
2025-03-27 08:49:13 -0500

    refactor: map styles eith stylesheet

--- app/map/index.tsx
+++ app/map/index.tsx
@@ -1,18 +1,28 @@
-import {View, Text} from "react-native";
-import {usePermissionsStore} from "@/presentation/store/usePermissions";
+import { View, Text, StyleSheet } from "react-native";
+import { usePermissionsStore } from "@/presentation/store/usePermissions";
 import MapView from "react-native-maps";
 
 const MapScreen = () => {
-    const {locationStatus} = usePermissionsStore();
-    return (
-        <View style={{ flex: 1}}>
-            <MapView  initialRegion={{
-                latitude: 37.78825,
-                longitude: -122.4324,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
-            }} style={{width: '100%', height: '100%'}}></MapView>
-        </View>
-    )
+  const {locationStatus} = usePermissionsStore();
+  return (
+      <View style={{flex: 1}}>
+        <MapView initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }} style={styles.map}/>
+      </View>
+  )
 }
 export default MapScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
