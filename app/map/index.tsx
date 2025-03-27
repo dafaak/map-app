294d52cf274ac34bc3540c9632d18be1271e6bfb
Israel <israel.pe@outlook.es>
2025-03-27 15:25:25 -0500

    feat: use new custom map comp and location store

--- app/map/index.tsx
+++ app/map/index.tsx
@@ -1,17 +1,41 @@
-import { View, Text, StyleSheet } from "react-native";
-import { usePermissionsStore } from "@/presentation/store/usePermissions";
+import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
+import { usePermissionsStore } from "@/presentation/store/usePermissionsStore";
 import MapView from "react-native-maps";
+import CustomMap from "@/presentation/components/maps/CustomMap";
+import { useLocationStore } from "@/presentation/store/useLocationStore";
+import { useEffect } from "react";
 
 const MapScreen = () => {
   const {locationStatus} = usePermissionsStore();
+
+  const {lastKnowLocation, getLocation} = useLocationStore();
+
+  useEffect(() => {
+    if (!lastKnowLocation) getLocation().catch(console.error);
+
+  }, [])
+
+
+  if (!lastKnowLocation) {
+    return (<View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>laSTlOCATION:{lastKnowLocation}</Text>
+      <ActivityIndicator/>
+    </View>)
+  }
+
   return (
       <View style={{flex: 1}}>
-        <MapView initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }} style={styles.map}/>
+
+        <CustomMap initialLocation={lastKnowLocation}
+                   showUserLocation={true}
+        ></CustomMap>
+
+        {/*<MapView initialRegion={{*/}
+        {/*  latitude: 37.78825,*/}
+        {/*  longitude: -122.4324,*/}
+        {/*  latitudeDelta: 0.0922,*/}
+        {/*  longitudeDelta: 0.0421,*/}
+        {/*}} style={styles.map}/>*/}
       </View>
   )
 }
